1de2a6294cf7714e31286fc222cb4dfe16640bc5
DSSKTOP-HONDA-H\shiro <s183611u@st.go.tuat.ac.jp>
2020-10-14 17:21:50 +0900

    v1.00

--- main.py
+++
@@ -1,9 +0,0 @@
-import tkinter
-from calcleaf.gui.app import AppForm
-
-
-root = tkinter.Tk()
-root.title("Leaf Image")
-root.option_add('*font', ('MS Sans Serif', 16))
-app = AppForm(master=root)
-app.mainloop()
